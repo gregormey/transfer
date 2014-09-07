0f306279d8f4ce3266e01b9a1b35982221a3cd39
Gregor Meyenberg <gregor@meyenberg.de>
2014-09-07 16:08:45 +0200

    upload post route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 , fs = require("fs")
 , swig = require('swig')
+, http = require('http')
+, formidable = require('formidable')
+, fs = require('fs')
 , app = express();
 
  // assign the swig engine to .html files
@@ -13,6 +16,38 @@ app.set('views', __dirname + '/views');
 
 app.get('/upload', function(req,res){
   res.render('upload');
+});
+
+
+app.post('/upload', function(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        // `file` is the name of the <input> field of type `file`
+        var old_path = files.file.path,
+            file_size = files.file.size,
+            file_ext = files.file.name.split('.').pop(),
+            index = old_path.lastIndexOf('/') + 1,
+            file_name = old_path.substr(index),
+            new_path = __dirname + '/uploads/'+ file_name + '.' + file_ext;
+ 
+        fs.readFile(old_path, function(err, data) {
+            fs.writeFile(new_path, data, function(err) {
+                fs.unlink(old_path, function(err) {
+                    if (err) {
+                        res.status(500);
+                        res.render('upload',
+                            { "error":true}
+                        );
+                    } else {
+                        res.status(200);
+                        res.render('upload',
+                            { "success":true}
+                        );
+                    }
+                });
+            });
+        });
+    });
 });
 
 
