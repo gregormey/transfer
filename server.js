879516a09b6de30b99ed6b0b7529acabf4a40233
Gregor Meyenberg <gregor@meyenberg.de>
2014-09-07 13:00:49 +0200

    upload form

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express')
 , fs = require("fs")
+, swig = require('swig')
 , app = express();
+
+ // assign the swig engine to .html files
+app.engine('html', swig.renderFile);
+
+
+// set .html as the default extension 
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/upload', function(req,res){
+  res.render('upload');
+});
+
+
 
 app.get('/:file', function(req, res){
   var file = __dirname + "/downloads/" + req.params.file;
@@ -14,6 +29,9 @@ app.get('/:file', function(req, res){
     res.download(file); // Set disposition and send it.
   });
 });
+
+
+
 
 app.listen(8888);
 console.log('File Server listening port 8888');
